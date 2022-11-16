5f495e6496542a6f3bb3ab1d1c21be1f1cb26a50
Jonathan Rubio <jonathan.rubio@wiedii.co>
2022-11-16 10:10:17 -0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import './bootstrap';
+
+window.Echo.private('notifications')
+    .listen('UserSessionChanged', (e) => {
+        
+        console.log("entra");
+        console.log(e);
+
+        const notificationElement = document.getElementById('notification');
+
+        notificationElement.innerText = e.message;
+
+        notificationElement.classList.remove('invisible');
+        notificationElement.classList.remove('alert-success');
+        notificationElement.classList.remove('alert-danger');
+
+        notificationElement.classList.add('alert-'+ e.type);
+    });
